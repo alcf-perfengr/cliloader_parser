8bd87b9674e99d8ed6e9493019a89b26bec5aba7
Brice Videau <brice.videau@imag.fr>
2019-07-25 12:51:47 -0500

    Refactoring.

--- lib/cliloader.rb
+++ lib/cliloader.rb
@@ -1,1 +1,4 @@
 require_relative 'cliloader/parser'
+require_relative 'cliloader/cl'
+
+CLILoader::Parser.generate
--- test/parser.rb
+++ test/parser.rb
@@ -2,7 +2,6 @@ class TestParser < Minitest::Test
   def test_3D
     File::open("./3D/clintercept_log.txt", "r") { |f|
       objects, events = CLILoader::Parser.parse(f)
-      pp objects
       assert_equal( 7, objects.size)
       assert_equal( 1527, events.size)
       objects.each { |_, ol|
